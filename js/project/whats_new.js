0c16a215d3feee6dd3843129aca466b460734554
Charles He <charles@hbtagency.com.au>
2016-01-11 11:53:42 +1100

    update frontend-layout, setup homepage templete, added DT for homepage templete

--- js/project/whats_new.js
+++ js/project/whats_new.js
@@ -1,17 +1,17 @@
-$(window).load(function(){
-    if(isSafari){
-        responsiveCarousel();
-        initCarouselItem();
-    }
-});
+// $(window).load(function(){
+//     if(isSafari){
+//         responsiveCarousel();
+//         initCarouselItem();
+//     }
+// });
 
 
-$(document).ready(function(){
-    if(isSafari === false){
-        responsiveCarousel();
-        initCarouselItem();
-    }
-});
+// $(window).load(function(){
+//     if(isSafari === false){
+//         responsiveCarousel();
+//         initCarouselItem();
+//     }
+// });
 
 $(".slick-carousel .item").hover(function(){
     $(this).children(".img-div").addClass("hovered");
@@ -24,44 +24,44 @@ $(".slick-carousel .item").mouseleave(function(){
 
 });
 
-function initCarouselItem(){
-    if(isSafari){
-        var parent_height = $(".whats_new_mask").parent().height();
-        $(".whats_new_mask").height(parent_height);
-    }else{
-        var parent_height = $(".whats_new_mask").first().parent().height();
-        $(".whats_new_mask").height(parent_height);
-    }  
-}
+// function initCarouselItem(){
+//     if(isSafari){
+//         var parent_height = $(".whats_new_mask").parent().height();
+//         $(".whats_new_mask").height(parent_height);
+//     }else{
+//         var parent_height = $(".whats_new_mask").first().parent().height();
+//         $(".whats_new_mask").height(parent_height);
+//     }  
+// }
 
-function responsiveCarousel() {
-    var window_width = $(window).width();
-    if (window_width < 520) {
-        $('.autoplay').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            prevArrow: $('.prev'),
-            nextArrow: $('.next'),
-        });
-    } else if (window_width < 992) {
-        $('.autoplay').slick({
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            prevArrow: $('.prev'),
-            nextArrow: $('.next'),
-        });
-    } else {
-        $('.autoplay').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            prevArrow: $('.prev'),
-            nextArrow: $('.next'),
-        });
-    }
-}
+// function responsiveCarousel() {
+//     var window_width = $(window).width();
+//     if (window_width < 520) {
+//         $('.autoplay').slick({
+//             slidesToShow: 1,
+//             slidesToScroll: 1,
+//             autoplay: true,
+//             autoplaySpeed: 2000,
+//             prevArrow: $('.prev'),
+//             nextArrow: $('.next'),
+//         });
+//     } else if (window_width < 992) {
+//         $('.autoplay').slick({
+//             slidesToShow: 2,
+//             slidesToScroll: 1,
+//             autoplay: true,
+//             autoplaySpeed: 2000,
+//             prevArrow: $('.prev'),
+//             nextArrow: $('.next'),
+//         });
+//     } else {
+//         $('.autoplay').slick({
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//             autoplay: true,
+//             autoplaySpeed: 2000,
+//             prevArrow: $('.prev'),
+//             nextArrow: $('.next'),
+//         });
+//     }
+// }
