9a0abac8a10c8065c79b85b49bba743acb5627bc
Charles He <charles@hbtagency.com.au>
2016-01-18 11:12:21 +1100

    filter angularjs

--- js/project/postAngularController.js
+++ js/project/postAngularController.js
@@ -1,9 +1,8 @@
 var mainApp = angular.module('mainApp',[]);
-mainApp.controller('postController',function ($scope, $location, postService){
+mainApp.controller('postController', function ($scope, $location, filterFilter, postService){
     
-    // alert($location.path());
-
-	getPosts();
+    
+	
 
 	function getPosts(){
 		postService.getPosts().success(function(data){
@@ -14,6 +13,14 @@ mainApp.controller('postController',function ($scope, $location, postService){
 			console.log(error);
 		})
 	}
+
+
+	getPosts();
+	
+	if ($location.path()=="/test") {
+		// var filter = $filter('filterB');
+		alert("test");
+    };
 });
 
 mainApp.factory('postService', ['$http', function ($http) {
@@ -26,3 +33,28 @@ mainApp.factory('postService', ['$http', function ($http) {
 
 }]);
 
+
+// mainApp
+// .constant('baseHref', '/en/newsevents/')
+// // .value('$sniffer', { history: false })
+
+
+// // .config(function($locationProvider) {
+// //   $locationProvider.html5Mode(true).hashPrefix('!');
+// // })
+
+// .controller("LocationController", function($scope, $location) {
+//   $scope.$location = {};
+//   angular.forEach("protocol host port path search hash".split(" "), function(method){
+//     $scope.$location[method] = function(){
+//       var result = $location[method].call($location);
+//       return angular.isObject(result) ? angular.toJson(result) : result;
+//     };
+//   });
+// });
+
+// // .run(function($rootElement) {
+// //   $rootElement.on('click', function(e) {
+// //     e.stopPropagation();
+// //   });
+// // });
