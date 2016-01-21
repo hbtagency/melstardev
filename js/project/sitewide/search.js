366208c8716dca5e2168fb10b120c6df99cf041e
Charles He <charles@hbtagency.com.au>
2016-01-21 17:32:31 +1100

    search

--- js/project/sitewide/search.js
+++ js/project/sitewide/search.js
@@ -1,8 +1,16 @@
 $(".search_button").click(function(){
 	$("#search-mask").addClass("show");
+	$('html').css({
+	    'overflow': 'hidden',
+	    'height': '100%'
+	});
 	$("#search-mask").fadeIn(1500);
 });
 
 $("#mask_close_button").click(function(){
 	$("#search-mask").removeClass("show");
+	$('html').css({
+	    'overflow-y': 'scroll',
+	    'height': 'auto'
+	});
 });
