7e6bf40db1bd3af9235399eb178f4ddecded88a6
Nathan Zhang <nathan.z@hbtagency.com.au>
2016-01-25 16:35:18 +1100

    sign up, ticket, share to wechat trial

--- js/project/sitewide/signup.js
+++ js/project/sitewide/signup.js
@@ -1,118 +1,92 @@
-﻿$(document).ready(function () {
-    ajaxMailChimpForm($("#subscribe-form"), $("#subscriberesult2"), $("#emailField"));
-    // Turn the given MailChimp form into an ajax version of it.
-    // If resultElement is given, the subscribe result is set as html to
-    // that element.
-    function ajaxMailChimpForm($form, $resultElement, $emailField, $mailContainer) {
-        // Hijack the submission. We'll submit the form manually.
-        $form.submit(function (e) {
-            e.preventDefault();
-            if (!isValidEmail($form)) {
-                var error = "A valid email address must be provided.";
-                $resultElement.html(error);
-            } else {
-                $resultElement.html("Subscribing...");
-                submitSubscribeForm($form, $resultElement, $emailField);
-            }
-        });
+﻿$("form#subscribe-form").submit(function (event) {
+    event.preventDefault();
+    
+    var form = "mc-embedded-subscribe-form";
+    var form_element = $("#subscribe-form");
+    var field_list = ["FNAME", "LNAME", "EMAIL"];
+    var error_msg = "";
+    var error_message_list = { required: "This field is required!", email: "Please enter a valid email!" };
+
+    if (validForm(form, field_list)) {
+        //Ajax call
+        var action = "http://melbournestar.us11.list-manage.com/subscribe/post-json?u=780de5edae0da9609d83bdfdc&amp;id=df97abedaf";
+        ajaxCallMailChimp(action, form_element);
     }
-    // Validate the email address in the form
-    function isValidEmail($form) {
-        return true;
-    }
-    // Submit the form with an ajax/jsonp request.
-    // Based on http://stackoverflow.com/a/15120409/215821
-    function submitSubscribeForm($form, $resultElement, $emailField) {
+
+    function ajaxCallMailChimp(action, form_obj) {
         $.ajax({
             type: "GET",
-            url: $form.attr("action"),
-            data: $form.serialize(),
+            url: action,
+            data: form_obj.serialize(),
             cache: false,
             dataType: "jsonp",
             jsonp: "c", // trigger MailChimp to return a JSONP response
             contentType: "application/json; charset=utf-8",
             error: function (error) {
                 // According to jquery docs, this is never called for cross-domain JSONP requests
-                alert("Mailchimp error");
+                $("#sign-up-msg").html("Mailchimp Error!");
             },
             success: function (data) {
-                if (data.result != "success") {
-                    var message = data.msg || "Sorry. Unable to subscribe. Please try again later.";
-                    if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
-                        $resultElement.html(message);
-                    }
-                    $resultElement.html(message);
-                    $resultElement.removeClass("hidden");
-                    $emailField.removeClass();
-                    $emailField.addClass("error");
-                    $emailField.val("");
-                    //$mailContainer.addClass("error");
-                } else {
-                    data.msg = "Success: Please check your email account to verify your email address"
-                    $resultElement.html(data.msg);
-                    $resultElement.addClass("success");
-                    $resultElement.removeClass("hidden");
-                    $emailField.removeClass();
-                    $emailField.addClass("success");
-                    $emailField.val("");
-                    //$mailContainer.addClass("success");
+                if (data.result == "success") {
+                    $("#sign-up-msg").addClass("green");
                 }
+                $("#sign-up-msg").html(data.msg);
+                $("#sign-up-msg").removeClass("hidden");
             }
         });
     }
+
+    function validForm(form, field_list) {
+        var validForm = true;
+        for (var field in field_list) {
+            var field_value = document.forms[form][field_list[field]].value;
+            var current_field = document.forms[form][field_list[field]];
+
+            //Null Validation 
+            if (!isNotNull(field_value)) {
+                error_msg = error_message_list.required;
+                validForm = false;
+            } else {
+                //Email Validation
+                if (field_list[field] == "EMAIL") {
+                    if (!isValidEmail(field_value)) {
+                        error_msg = error_message_list.email;
+                        validForm = false;
+                    }
+                }
+            }
+
+            if (error_msg != "") {
+                //Gonna show error messages per field
+                $("input[name='" + current_field.name + "']").siblings(".error-msg").html(error_msg);
+                $("input[name='" + current_field.name + "']").siblings(".error-msg").removeClass("hidden");
+            }
+        }
+        return validForm;
+    }
+
+    function isNotNull(field) {
+        if ((field != undefined) && (field != null) && (field != "")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function isValidEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
 });
 
-
-//Expanable details
-var windowWidth = window.innerWidth;
-
-
-$('html').click(function (e) {
-    if (e.target.id == 'emailField' || e.target.id == 'extra_form_1' || e.target.id == 'extra_form_2' || e.target.id == 'extra_form_3' || e.target.id == 'extra_form_4' || e.target.id == 'extra_form_5' || e.target.id == 'mce-MMERGE' || e.target.id == 'signup_submit' || e.target.id == 'mce-MMERGE3-month' || e.target.id == 'mce-MMERGE3-day') {
-        $(this).val("");
-        $(this).removeClass();
-
-        $("#subscriberesult2").removeClass("success");
-        $("#subscriberesult2").addClass("hidden");
-        $("#subscriberesult2").html("");
-        $("#emailField").removeClass("error");
-
-        $("#extra_wrapper").css({
-            "height": "auto"
-        });
-
-        $(".extra_Info").css({
-            "max-height": "300px"
-        });
-        $(".sign_up").css({
-            "height": "auto"
-        });
-
+$("#subscribe-form input[type=text]").click(function () {
+    if (!$("#sign-up-msg").hasClass("hidden")) {
+        $("#sign-up-msg").addClass("hidden");
     }
-    else {
-        $("#extra_wrapper").css({
-            "height": "auto",
-        });
 
-        $(".extra_Info").css({
-            "-webkit-transition": "max-height 1s",
-            "-moz-transition": " max-height 1s",
-            "-ms-transition": "max-height 1s",
-            "-o-transition": " max-height 1s",
-            "transition": "max-height 1s",
-            "overflow": "hidden",
-            "max-height": " 0px"
-        });
-
-        $(".sign_up").css({
-            "height": "auto",
-            "overflow": "auto"
-        });
-
-        $("#signup_button").css({
-            "top": "0",
-            "position": "relative",
-            "right": "0",
-        });
-    }
+    $(".error-msg").each(function () {
+        if (!$(this).hasClass("hidden")) {
+            $(this).addClass("hidden");
+        }
+    })
 });
