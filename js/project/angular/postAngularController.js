fc0b5679ca3c6c615c88541856a244f4c8aec67f
Charles He <charles@hbtagency.com.au>
2016-02-01 16:38:12 +1100

    fix instagram 404 error & part of no HTML isuue

--- js/project/angular/postAngularController.js
+++ js/project/angular/postAngularController.js
@@ -119,40 +119,7 @@ mainApp.directive('ngRepeatEndWatch', function () {
 });
 
 
-
-
-
-
-
-
-
-
-
 angular.element(document).ready(function() {
     angular.bootstrap(document.getElementById('mainApp'), ['mainApp']);
 });
 
-// mainApp
-// .constant('baseHref', '/en/newsevents/')
-// // .value('$sniffer', { history: false })
-
-
-// // .config(function($locationProvider) {
-// //   $locationProvider.html5Mode(true).hashPrefix('!');
-// // })
-
-// .controller("LocationController", function($scope, $location) {
-//   $scope.$location = {};
-//   angular.forEach("protocol host port path search hash".split(" "), function(method){
-//     $scope.$location[method] = function(){
-//       var result = $location[method].call($location);
-//       return angular.isObject(result) ? angular.toJson(result) : result;
-//     };
-//   });
-// });
-
-// // .run(function($rootElement) {
-// //   $rootElement.on('click', function(e) {
-// //     e.stopPropagation();
-// //   });
-// // });
