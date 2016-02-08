27903f1c5e6587dc68089746564e3eb2ef594c1a
Nathan Zhang <nathan.z@hbtagency.com.au>
2016-02-08 15:57:32 +1100

    responsive fix

--- js/project/banner_mask_height_init.js
+++ js/project/banner_mask_height_init.js
@@ -2,19 +2,12 @@
 $(document).ready(function(){
     if($(window).width() < 1200){
         var container_height = $(".box-center-container").height();
-        if (isSafari){
-            $(".banner_area .mask").height(container_height);
-        }
-        
     }
 });
 
 
 $(window).resize(function(){
-    if($(window).width() < 1200){
+    if($(window).width() < 1200){ 
         var container_height = $(".box-center-container").height();
-        if (isSafari){
-            $(".banner_area .mask").height(container_height);
-        }
     }
 });
