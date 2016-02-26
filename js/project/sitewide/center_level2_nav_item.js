4b175541ebb379c214d4a50498c0634fde85922a
Nathan Zhang <nathan.z@hbtagency.com.au>
2016-02-26 13:58:25 +1100

    main nav min 100% width style for level2 item

--- js/project/sitewide/center_level2_nav_item.js
+++ js/project/sitewide/center_level2_nav_item.js
@@ -5,9 +5,6 @@
         /*Step 2, introduce CenterChildNavAnimator to do the centering job.*/
         var animator = new CenterChildNavAnimator(parentWidth);
         animator.centerDiv($(this), "l");
-        if ($(this).width() < parentWidth) {
-            $(this).css("width", (parentWidth) + "px");
-        }
     });
 });
 
