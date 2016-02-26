d2df5bed55d4b9c5c9a12c25d62a0cf7f97575f5
Nathan Zhang <nathan.z@hbtagency.com.au>
2016-02-26 13:44:57 +1100

    education new structure

--- js/project/sitewide/center_level2_nav_item.js
+++ js/project/sitewide/center_level2_nav_item.js
@@ -5,6 +5,9 @@
         /*Step 2, introduce CenterChildNavAnimator to do the centering job.*/
         var animator = new CenterChildNavAnimator(parentWidth);
         animator.centerDiv($(this), "l");
+        if ($(this).width() < parentWidth) {
+            $(this).css("width", (parentWidth) + "px");
+        }
     });
 });
 
