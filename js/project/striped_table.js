203f5adb140601bead905dc5385aebb43cef0263
Nathan Zhang <nathan.z@hbtagency.com.au>
2016-03-15 13:53:29 +1100

    oo striped table.js

--- js/project/striped_table.js
+++ js/project/striped_table.js
@@ -1,13 +1,27 @@
 $(document).ready(function () {
     var striped_table = $("#table-striped-inside table tr");
-    var counter = 0; 
+    /*var counter = 0; 
     striped_table.each(function(){
         if(counter%2 === 0 ){
             $(this).addClass("striped"); 
         }
         counter++;
-    });
+    });*/
+    var tableStripToo = new TableStripTool(striped_table);
+    tableStripToo.stripeTable("striped");
 });
 
+var TableStripTool = function (table_element) {
+    var table;
+    this.table = table_element;
+}
 
-
+TableStripTool.prototype.stripeTable = function (className) {
+    var counter = 0; 
+    this.table.each(function () {
+        if (counter % 2 === 0) {
+            $(this).addClass(className);
+        }
+        counter++;
+    });
+}
